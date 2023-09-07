392fe69b6498663a215e0e2ca091cbc8e6be8ce9
David Amey <djamey87@gmail.com>
2023-09-07 11:10:01 +0100

    :construction: adding bottom drawer to house the input form

--- App.tsx
+++ App.tsx
@@ -1,11 +1,28 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { useRef } from "react";
+import { StyleSheet, Text, View, Button } from "react-native";
+import BottomDrawer, {
+  BottomDrawerMethods,
+} from "react-native-animated-bottom-drawer";
 
 export default function App() {
+  const bottomDrawerRef = useRef<BottomDrawerMethods>(null);
   return (
     <View style={styles.container}>
       <Text>Open up App.tsx to start working on your app!</Text>
+
+      <Button
+        title="Add Quote"
+        onPress={() => bottomDrawerRef.current?.open()}
+      />
       <StatusBar style="auto" />
+
+      <BottomDrawer ref={bottomDrawerRef}>
+        <View style={styles.contentContainer}>
+          <Text>Awesome!</Text>
+          {/* Add form here */}
+        </View>
+      </BottomDrawer>
     </View>
   );
 }
@@ -13,8 +30,12 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  contentContainer: {
+    flex: 1,
+    alignItems: "center",
   },
 });
